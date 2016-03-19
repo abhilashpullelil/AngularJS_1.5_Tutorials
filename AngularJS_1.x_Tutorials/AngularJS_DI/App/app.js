ace5dafed4d1b2f00cb942980ca946415ee8af3d
Pullelil_Home <abhilashpullelil@yahoo.com>
2016-03-19 17:15:38 +0530

    Added sample 2 - Dependency Injection

---
+++ AngularJS_1.x_Tutorials/AngularJS_DI/App/app.js
@@ -0,0 +1,34 @@
+﻿
+angular.module('myApp', ['coreModule']);
+
+angular.module('myApp').controller('firstController', ['$scope', 'version', '$http', 'baseUrl', 'sharedData', function ($scope, version, $http, baseUrl, sharedData) {
+    debugger;
+    $scope.cntVersion = version;
+
+    $scope.UserName = sharedData.UserName;
+    $scope.Course = sharedData.Course;
+
+    // Functions
+
+    $scope.updateUserdata = function () {
+        sharedData.UpdateUserName($scope.UserName);
+    }
+    
+    $scope.$watch('UserName', function () {
+        sharedData.UpdateUserName($scope.UserName);
+    });
+}])
+.controller('secondController', ['$scope', 'firstVersion', 'sharedData', function ($scope, firstVersion, sharedData) {
+    $scope.baseVersion = firstVersion;
+
+    $scope.name = sharedData.UserName;
+
+    //$scope.updateNameFromFactory = function () {
+    //    $scope.name = sharedData.UserName;
+    //}
+
+    $scope.$on('name_has_been_changed', function (event, params) {
+        debugger;
+        $scope.name = sharedData.UserName;
+    });
+}]);
---
+++ AngularJS_1.x_Tutorials/AngularJS_DI/App/secondModule.js
@@ -0,0 +1,20 @@
+﻿
+
+angular.module('coreModule', [])
+    .value('version', 4.5)
+
+    .constant('firstVersion', 1.0)
+
+    .constant('baseUrl', 'http://deve/api/')
+
+    .factory('sharedData', function ($rootScope) {
+        var myFacory = {};
+        myFacory.UserName = "Aroma";
+        myFacory.Course = "AngularJS 1.5";
+        //Update the user name
+        myFacory.UpdateUserName = function (name) {
+            myFacory.UserName = name;
+            $rootScope.$broadcast('name_has_been_changed', {myId : 12});
+        }
+        return myFacory;
+    });
